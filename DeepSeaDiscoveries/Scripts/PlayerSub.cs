c15f2bbe29025119a8b841c1764c91f720512ee5
Kevin Van Heusen <lithiastudios@gmail.com>
2021-04-23 22:33:04 -0700

    retractable hook

--- DeepSeaDiscoveries/Scripts/PlayerSub.cs
+++ DeepSeaDiscoveries/Scripts/PlayerSub.cs
@@ -4,14 +4,20 @@ using System;
 public class PlayerSub : KinematicBody2D
 {
 	private const int MOVE_SPEED = 250;
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
+	private Node2D Hook;
+	private Tween HookLaunch;
+	private Tween HookRetract;
+
+	private bool Moveable;
+	private Vector2 CurrentHookPosition;
+	
 	public override void _Ready()
 	{
-		
+		Moveable = true;
+		Hook = GetNode<Node2D>("Hook");
+		HookLaunch = GetNode<Tween>("HookLaunch");
+		HookRetract = GetNode<Tween>("HookRetract");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,16 +25,45 @@ public class PlayerSub : KinematicBody2D
 	{
 		Vector2 playerVec = new Vector2();
 
-		if(Input.IsActionPressed("ui_left"))
+		if(Input.IsActionPressed("ui_left") && Moveable)
 		{
 			playerVec = new Vector2(-1, 0);
 		}
 		else
-			if(Input.IsActionPressed("ui_right"))
+			if(Input.IsActionPressed("ui_right")  && Moveable)
 			{
 				playerVec = new Vector2(1, 0);
 			}
+			else
+			if(Input.IsActionJustPressed("ui_accept") || Input.IsActionJustPressed("ui_select") && Moveable)
+			{
+			GD.Print("activated");
+			var currentHookPos = Hook.Position;
 
+			CurrentHookPosition = currentHookPos;
+			HookLaunch.InterpolateProperty(Hook, "position", currentHookPos, new Vector2(currentHookPos.x, currentHookPos.y + 100), 1, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+			HookLaunch.Start();
+			}
 		MoveAndCollide(playerVec * delta * MOVE_SPEED);
 	}
+	
+	
+	private void _on_HookLaunch_tween_completed(Godot.Object @object, NodePath key)
+	{
+		GD.Print("retracting..");
+		var currentHookPos = Hook.Position;
+		HookRetract.InterpolateProperty(Hook, "position", currentHookPos, new Vector2(CurrentHookPosition.x, CurrentHookPosition.y), 1, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+		HookRetract.Start();
+		// Replace with function body.
+	}
+	
+	
+private void _on_HookRetract_tween_completed(Godot.Object @object, NodePath key)
+{
+		Moveable = true;
 }
+
+}
+
+
+
